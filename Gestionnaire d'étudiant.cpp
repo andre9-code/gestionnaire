2aeb1e0c3e7440aebf740b06b2de64ed47c130de
andre9-code <andreessono@icloud.com>
2024-12-01 23:44:33 +0100

    Add files via upload

---
+++ Gestionnaire d'étudiant.cpp
@@ -0,0 +1,37 @@
+#include <string>
+#include <vector>
+#include <iostream>
+
+class Etudiant {
+private:
+    std::string nom;
+    std::string prenom;
+    int numeroEtudiant;
+    std::vector<float> notes; 
+
+public:
+
+    Etudiant(std::string n, std::string p, int num) : nom(n), prenom(p), numeroEtudiant(num) {}
+
+
+    void ajouterNote(float note) { notes.push_back(note); }
+
+    void afficherInfos() const {
+        std::cout << "Nom : " << nom << std::endl;
+        std::cout << "Prénom : " << prenom << std::endl;
+        std::cout << "Numéro étudiant : " << numeroEtudiant << std::endl;
+        std::cout << "Notes : ";
+        for (float note : notes) {
+            std::cout << note << " ";
+        }
+        std::cout << std::endl;
+    }
+
+
+    std::string getNom() const { return nom; }
+    std::string getPrenom() const { return prenom; }
+    int getNumeroEtudiant() const { return numeroEtudiant; }
+    std::vector<float> getNotes() const {return notes;}
+
+
+}
